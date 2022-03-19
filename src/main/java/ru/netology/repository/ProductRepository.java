f2d7d47e5459a6b78e77ad0e3ee096568ad0f8b1
natabask <natabask@yandex.ru>
2022-03-19 09:57:53 +0300

    exception class added

--- src/main/java/ru/netology/repository/ProductRepository.java
+++ src/main/java/ru/netology/repository/ProductRepository.java
@@ -1,5 +1,6 @@
 package ru.netology.repository;
 
+import ru.netology.domain.NotFoundException;
 import ru.netology.domain.Product;
 
 public class ProductRepository {
@@ -18,7 +19,19 @@ public class ProductRepository {
         return items;
     }
 
+    public Product findById(int id) {
+        for (Product item : items) {
+            if (item.getId() == id) {
+                return item;
+            }
+        }
+        return null;
+    }
+
     public void removeById(int id) {
+        if (findById(id) == null) {
+            throw new NotFoundException(id);
+        }
         int length = items.length - 1;
         Product[] tmp = new Product[length];
         int index = 0;
--- src/test/java/ru/netology/repository/ProductRepositoryTest.java
+++ src/test/java/ru/netology/repository/ProductRepositoryTest.java
@@ -1,7 +1,10 @@
 package ru.netology.repository;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ru.netology.domain.Book;
+import ru.netology.domain.NotFoundException;
 import ru.netology.domain.Product;
 import ru.netology.domain.Smartphone;
 
@@ -12,35 +15,32 @@ class ProductRepositoryTest {
     private Product first = new Book(1, "Capital", 500, "Karl Marx");
     private Product second = new Smartphone(2, "3110", 870, "Nokia");
     private Product third = new Book(3, "Eugene Onegin", 400, "Alexander Pushkin");
-    private Product fourth = new Smartphone(4, "Honor", 2000, "HTC");
 
-    @Test
-    // Сохранение всех продуктов
-    public void shouldSearchById() {
+    @BeforeEach
+    public void shouldSetInitialState() {
         repository.save(first);
         repository.save(second);
         repository.save(third);
-        repository.save(fourth);
+    }
+
+    @Test
+    // Удаление продукта по существующему id
+    public void shouldRemoveByExistentId() {
+
+        repository.removeById(2);
 
         Product[] actual = repository.findAll();
-        Product[] expected = new Product[]{first, second, third, fourth};
+        Product[] expected = new Product[]{first, third};
 
         assertArrayEquals(actual, expected);
     }
 
     @Test
-    // Удаление продукта по id
-    public void shouldRemoveById() {
-        repository.save(first);
-        repository.save(second);
-        repository.save(third);
-        repository.save(fourth);
+    // Удаление продукта по несуществующему id
+    public void shouldRemoveByNonExistentId() {
 
-        repository.removeById(3);
-
-        Product[] actual = repository.findAll();
-        Product[] expected = new Product[]{first, second, fourth};
-
-        assertArrayEquals(actual, expected);
+        assertThrows(NotFoundException.class, () -> {
+            repository.removeById(23);
+        });
     }
 }
